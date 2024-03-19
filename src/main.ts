886db38aafbeb2d975a6639d72365709f09c2bb0
AngeLund <lundmark.angelica@gmail.com>
2024-03-19 18:12:38 +0100

    html, javascript, css

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,20 @@
 console.log("Hello");
 
+
+
+function generateHeader (a) {
+    const ulEl = document.createElement('ul')as HTMLUListElement;
+    const liEl = document.createElement('li')as HTMLLIElement;
+    const liEl2 = document.createElement('li')as HTMLLIElement;
+    const liEl3 = document.createElement('li')as HTMLLIElement;
+    const divHead = document.getElementById('ul-header')as HTMLDivElement;
+
+    divHead.appendChild(ulEl);
+    ulEl.appendChild(liEl);
+    ulEl.appendChild(liEl2);
+    ulEl.appendChild(liEl3);
+    liEl.innerText = 'home';
+    liEl.innerText = 'profile';
+    liEl.innerText = 'users';
+
+}
