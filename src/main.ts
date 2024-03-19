02cf0b0378dd43706bbc5f6c36bb8b022de2b679
lunayr <144939306+lunayr@users.noreply.github.com>
2024-03-19 08:54:50 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,20 @@
+<<<<<<< Updated upstream
 console.log("Hello");
 
+=======
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json(), cors());
+
+
+
+
+
+
+app.listen(1234, () => {
+  console.log("listening on port 1234");
+});
+>>>>>>> Stashed changes
