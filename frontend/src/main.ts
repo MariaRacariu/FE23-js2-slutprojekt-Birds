e2920ac5bbbf0a6fc05dfd8f660250e69e76935f
AngeLund <lundmark.angelica@gmail.com>
2024-03-20 19:14:40 +0100

    Fixed change

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,18 +3,18 @@
 
 
 function generatProfil (t):void {
-const div = document.getElementById('show-logInn')as HTMLDivElement;
-const h5El = document.createElement('h5')as HTMLHeadingElement;
-const imgEl = document.createElement('img')as HTMLImageElement;
-const ulEl = document.createElement('ul')as HTMLUListElement;
-const liEl = document.createElement('li')as HTMLLIElement;
+  const div = document.getElementById('show-logInn')as HTMLDivElement;
+  const h5El = document.createElement('h5')as HTMLHeadingElement;
+  const imgEl = document.createElement('img')as HTMLImageElement;
+  const ulEl = document.createElement('ul')as HTMLUListElement;
+  const liEl = document.createElement('li')as HTMLLIElement;
 
-div.appendChild(h5El);
-div.appendChild(imgEl);
-div.appendChild(ulEl);
-ulEl.appendChild(liEl);
+  div.appendChild(h5El);
+  div.appendChild(imgEl);
+  div.appendChild(ulEl);
+  ulEl.appendChild(liEl);
 
-h5El.innerText = `Profile: $`
+  h5El.innerText = `Profile: $`
 
 }
 
@@ -27,17 +27,3 @@ function generateLogin (v):void {
   div.classList.remove("show-logIn");
 
 }
-
-/*const buttonLog = document.getElementById('idBu')as HTMLButtonElement; //Same med sign up knappen;
-buttonLog.addEventListener('click', generateLogin);
-
-function generateLogin (v):void {
-  const div = document.getElementById('show-logInn')as HTMLDivElement;
-  div.classList.remove("show-logIn");
-
-}/*
-
-
-
-
-
