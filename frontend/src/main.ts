4538f228354478b483e4ff19994184864dfabcbd
AngeLund <lundmark.angelica@gmail.com>
2024-03-20 19:11:56 +0100

    added javascript hidden

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,19 +1,43 @@
-/*function generateHeader (a) {
-  const ulEl = document.createElement('ul')as HTMLUListElement;
-  const liEl = document.createElement('li')as HTMLLIElement;
-  const liEl2 = document.createElement('li')as HTMLLIElement;
-  const liEl3 = document.createElement('li')as HTMLLIElement;
-  const divHead = document.getElementById('ul-header')as HTMLDivElement;
 
-  divHead.appendChild(ulEl);
-  ulEl.appendChild(liEl);
-  ulEl.appendChild(liEl2);
-  ulEl.appendChild(liEl3);
-  liEl.innerText = 'home';
-  liEl.innerText = 'profile';
-  liEl.innerText = 'users';
 
-}*/
+
+
+function generatProfil (t):void {
+const div = document.getElementById('show-logInn')as HTMLDivElement;
+const h5El = document.createElement('h5')as HTMLHeadingElement;
+const imgEl = document.createElement('img')as HTMLImageElement;
+const ulEl = document.createElement('ul')as HTMLUListElement;
+const liEl = document.createElement('li')as HTMLLIElement;
+
+div.appendChild(h5El);
+div.appendChild(imgEl);
+div.appendChild(ulEl);
+ulEl.appendChild(liEl);
+
+h5El.innerText = `Profile: $`
+
+}
+
+
+const buttonLog = document.getElementById('idBu')as HTMLButtonElement;
+buttonLog.addEventListener('click', generateLogin);
+
+function generateLogin (v):void {
+  const div = document.getElementById('show-logInn')as HTMLDivElement;
+  div.classList.remove("show-logIn");
+
+}
+
+/*const buttonLog = document.getElementById('idBu')as HTMLButtonElement; //Same med sign up knappen;
+buttonLog.addEventListener('click', generateLogin);
+
+function generateLogin (v):void {
+  const div = document.getElementById('show-logInn')as HTMLDivElement;
+  div.classList.remove("show-logIn");
+
+}/*
+
+
 
 
 
