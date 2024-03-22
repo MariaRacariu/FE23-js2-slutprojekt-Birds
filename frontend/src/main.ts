72ad98bc32c259c561521d70b9af51265b5f2d2a
MariaRacariu <90383391+MariaRacariu@users.noreply.github.com>
2024-03-22 15:47:57 +0100

    Revert "Merge branch 'main' into marias"
    This reverts commit 2f9fb15d85778a318272a9a4f59b1e9c5fbeb357, reversing
    changes made to 46c14197b1b6f9b021d3e2b7f7acf4c95ff2d26c.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,73 +1,62 @@
-import { content, profile } from "./modules/constant.ts";
-import { generateLoginContainer } from "./modules/login.ts";
-
-// Display log in form
-const buttonLogIn = document.querySelector("#logInButtonTriggerContainer") as HTMLButtonElement;
-buttonLogIn.addEventListener('click', (event) => {
-  event.preventDefault();
-  generateLoginContainer();
-});
-
-// This will need to be in a module
-const buttonSignUp = document.getElementById('signup-button') as HTMLButtonElement;
-// buttonSignUp.addEventListener('click', generateProfil);
+import { content, profile } from "./modules/constant";
 
 //mockup user, 
 const user = {
   name: 'Pia',
-  posts: [
+  posts:[
     {
-      message: 'test test'
+      message:'test test'
     },
     {
-      message: 'message 2'
+      message:'message 2'
     },
-  ]
-}
+]
+} 
 const users = {
-  "luna": {
-    "name": "Luna Berg",
-    "password": "123",
-    "profile_pic": "image1"
-  },
-  "testson": {
-    "name": "Test Testson",
-    "password": "test",
-    "profile_pic": "image1"
-  }
-}
+    "luna": {
+      "name": "Luna Berg",
+      "password": "123",
+      "profile_pic": "image1"
+    },
+    "testson": {
+      "name": "Test Testson",
+      "password": "test",
+      "profile_pic": "image1"
+    }
+} 
 
 //Hide all content boxes
-// Void? why you are not taking anything in
-export function hideAllContentBoxes(): void {
+function hideAllContentBoxes():void {
   document.querySelectorAll(`.${content.name}`).forEach(contentBox => {
     contentBox.classList.remove(content.isActive);
   })
 }
 
-// This whole function needs to be in a module
-// Better variable names
-// Void? why you are not taking anything in
-function generateProfil(): void {
+function generateLogin ():void {
+  hideAllContentBoxes()
+  const div = document.getElementById('show-logInn')as HTMLDivElement;
+  div.classList.add(content.isActive);
+  
+}
+
+function generateProfil ():void {
   hideAllContentBoxes();
   generateCategory();
   generateInput();
   //TODO: Fetch user
   //user = fetchUser
-  const div = document.getElementById(profile.id) as HTMLDivElement;
-  const h5El = document.getElementById(profile.name) as HTMLHeadingElement;
-  const imgEl = document.getElementById(profile.image) as HTMLImageElement;
-  const ulEl = document.getElementById(profile.posts) as HTMLUListElement;
+  const div = document.getElementById(profile.id)as HTMLDivElement;
+  const h5El = document.getElementById(profile.name)as HTMLHeadingElement;
+  const imgEl = document.getElementById(profile.image)as HTMLImageElement;
+  const ulEl = document.getElementById(profile.posts)as HTMLUListElement;
 
   h5El.innerText = `Profile: ${user.name}`;
-  imgEl.setAttribute('src', '#');
-  generatePosts(ulEl, user.posts)
+  imgEl.setAttribute('src','#');
+  generatePosts (ulEl, user.posts)
   div.classList.add(content.isActive);
 }
 
-// This function needs to be in a module
-// Void? why you are not taking anything in
-function generatePosts(ulEl: HTMLUListElement, posts: any[]): void {
+function generatePosts(ulEl:HTMLUListElement, posts:any[]):void{
   posts.forEach(post => {
     const liEl = document.createElement('li') as HTMLLIElement;
     liEl.innerText = post.message;
@@ -75,16 +64,17 @@ function generatePosts(ulEl: HTMLUListElement, posts: any[]): void {
   })
 }
 
+const buttonLog = document.getElementById('idBu')as HTMLButtonElement;
+buttonLog.addEventListener('click', generateLogin);
+
+const buttonSignUp = document.getElementById('signup-button')as HTMLButtonElement;
+buttonSignUp.addEventListener('click', generateProfil,);
 
 
-
-
-// This needs to be a module
-// Void? why you are not taking anything in
-function generateCategory(): void {
-  hideAllContentBoxes();
-  const div = document.getElementById('forum-container');
-  div?.classList.add(content.isActive);
+function generateCategory ():void{
+hideAllContentBoxes();
+const div = document.getElementById('forum-container');
+div?.classList.add(content.isActive);
 }
 
 
@@ -117,34 +107,34 @@ const posts = [
     "created_at": 1710974902
   }
 ]
-// This needs to be a module
-// Void? why you are not taking anything in
-function getPost(): void {
-  const ulEl = document.getElementById('post-ul') as HTMLUListElement;
-  posts.forEach(post => {
-    const liEl = document.createElement('li') as HTMLLIElement;
-    const authorP = document.createElement('p') as HTMLParagraphElement;
-    const titleP = document.createElement('p') as HTMLParagraphElement;
-    const bodyP = document.createElement('p') as HTMLParagraphElement;
-    ulEl.appendChild(liEl);
-    liEl.appendChild(authorP);
-    liEl.appendChild(titleP);
-    liEl.appendChild(bodyP);
-    authorP.innerText = post.author;
-    titleP.innerText = post.title;
-    bodyP.innerText = post.body;
-  }
 
-  )
+function getPost ():void{
+const ulEl = document.getElementById('post-ul')as HTMLUListElement;
+posts.forEach(post =>{
+const liEl = document.createElement('li')as HTMLLIElement;
+const authorP = document.createElement('p')as HTMLParagraphElement;
+const titleP = document.createElement('p')as HTMLParagraphElement;
+const bodyP = document.createElement('p')as HTMLParagraphElement;
+ulEl.appendChild(liEl);
+liEl.appendChild(authorP);
+liEl.appendChild(titleP);
+liEl.appendChild(bodyP);
+authorP.innerText = post.author;
+titleP.innerText = post.title;
+bodyP.innerText = post.body;
+}
+
+  ) 
 
 }
 
-const buttonPost = document.getElementById('post-button') as HTMLButtonElement;
+const buttonPost = document.getElementById('post-button')as HTMLButtonElement;
 buttonPost.addEventListener('click', getPost);
 
-// Void? why you are not taking anything in
-function generateInput(): void {
+function generateInput ():void{
   //hideAllContentBoxes();
-  const divEl = document.getElementById('input-field') as HTMLDivElement;
+  const divEl = document.getElementById('input-field')as HTMLDivElement;
   divEl.classList.add(content.isActive);
-}
+  }
+
+
--- frontend/src/modules/login.ts
+++
@@ -1,8 +0,0 @@
-import { hideAllContentBoxes } from "../main.ts";
-import { content} from "./modules/constant.ts";
-
-export function generateLoginContainer() {
-    hideAllContentBoxes();
-    const div = document.querySelector("#displayLoginContainer") as HTMLDivElement;
-    div.classList.add(content.isActive);
-}
